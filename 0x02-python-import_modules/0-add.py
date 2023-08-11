3cdbab7b2201f73a54fc010f0e0c5a665b8869e8
koome <koomemc@gmail.com>
2023-08-11 16:20:53 -0700

    main guard

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import add_0
+    from add_0 import add
     a = 1
     b = 2
-    sum = add_0.add(a, b)
-    print(f"{a:d} + {b:d} = {sum:d}")
+    print(f"{a:d} + {b:d} = {add(a,b)}")
--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-add = add(a, b)
-sub = sub(a, b)
-mul = mul(a, b)
-div = div(a, b)
-print(f"{a:d} + {b:d} = {add:d}\n{a:d} - {b:d} = {sub:d}\n{a:d} * {b:d} = {mul:d}\n{a:d} / {b:d} = {div:d}")
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print(f"{a:d} + {b:d} = {add(a, b)}\n{a:d} - {b:d} = {sub(a, b)}\n{a:d} * {b:d} = {mul(a, b)}\n{a:d} / {b:d} = {div(a, b)}")
