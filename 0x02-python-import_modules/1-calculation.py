f7fd109823c16fda7b5b4315a33665583a9b7b6a
koome <koomemc@gmail.com>
2023-08-11 18:15:23 -0700

    4 print lines instead of 1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,4 +3,7 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     a = 10
     b = 5
-    print(f"{a:d} + {b:d} = {add(a, b)}\n{a:d} - {b:d} = {sub(a, b)}\n{a:d} * {b:d} = {mul(a, b)}\n{a:d} / {b:d} = {div(a, b)}")
+    print(f"{a:d} + {b:d} = {add(a, b)}")
+    print(f"{a:d} - {b:d} = {sub(a, b)}")
+    print(f"{a:d} * {b:d} = {mul(a, b)}")
+    print(f"{a:d} / {b:d} = {div(a, b)}")
