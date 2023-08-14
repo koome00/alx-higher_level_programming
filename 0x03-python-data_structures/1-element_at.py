b80a65822ccd84374d10688b55a0228667433ed9
koome <koomemc@gmail.com>
2023-08-14 09:53:27 -0700

    initial commit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,7 @@ def element_at(my_list, idx):
     list_len = len(my_list)
     if idx < 0:
         print("None")
-    elif idx > (list_len - 1):
+    elif idx >= list_len:
         print("None")
     else:
         num = my_list[idx]
