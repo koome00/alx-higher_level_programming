f51f54cf2693c9160515c70447ad5a7c1794cb53
koome <koomemc@gmail.com>
2023-08-14 12:37:47 -0700

    fix style

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
     letters = "cC"
-    new_string = "".join(letter for letter in my_string if letter not in letters)
-    print(new_string)
+    new_string = "".join(
+            letter for letter in my_string if letter not in letters)
+    return new_string
