726f8074601f8aa783d2ddc6a6242b5bdca411ee
koome <koomemc@gmail.com>
2023-08-14 17:52:35 -0700

    fix None in task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,9 +2,9 @@
 def element_at(my_list, idx):
     list_len = len(my_list)
     if idx < 0:
-        print("None")
+        print(None)
     elif idx >= list_len:
-        print("None")
+        print(None)
     else:
         num = my_list[idx]
         return num
