e6ed4aa203c36b9ce81e5e2a627fb10cfec13189
koome <koomemc@gmail.com>
2023-08-14 17:42:45 -0700

    fix 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,6 @@ def multiple_returns(sentence):
     first = sentence[0]
     if length == 0:
         tuple_a = tuple((length, None))
-        return tuple_a
     else:
         tuple_a = tuple((length, first))
-        return tuple_a
+    return tuple_a
