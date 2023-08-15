fdeb6909d6697481b90eb488dfe5463d3f48ed22
koome <koomemc@gmail.com>
2023-08-14 17:30:54 -0700

    initial commit

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
     length = len(my_list)
-    if 0 > idx > length:
+    if 0 >= idx > length:
         my_list = my_list
     else:
         del my_list[idx]
