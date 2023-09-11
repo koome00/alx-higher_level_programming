86392a21363cc37963007747fa2bbc3c0375d7ca
koome <koomemc@gmail.com>
2023-09-11 11:10:24 -0700

    initial commit

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module 4-inherits_from
+
+Function definition that checks subclass
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Returns:
+        True if the object is an instance of a class
+        that inherited (directly or indirectly) from
+        the specified class ; otherwise False.
+    """
+    return issubclass(obj, a_class)
