201d79e0b31888626ecd2b05f94b4df2fa873e68
koome <koomemc@gmail.com>
2023-09-11 15:08:31 -0700

    initial commit

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module 5-base_geometry
+
+Contains an empty class definition
+"""
+
+
+class BaseGeometry():
+    """
+    Empty class definition
+    """
+    pass
---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+
+Defines class BaseGeometry with
+public instance method
+"""
+
+
+class BaseGeometry():
+    """
+    Methods:
+        area(self)
+    """
+    def area(self):
+        """ raises an exception"""
+        raise Exception("area() is not implemented")
