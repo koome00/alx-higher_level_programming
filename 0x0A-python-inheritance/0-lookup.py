924a0a2c52cff5e22a45091c8717f497fdc661a4
koome <koomemc@gmail.com>
2023-09-11 07:54:33 -0700

    initial commit

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 0-lookup
+Lookks up attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """returns a ist of objects's attributes and methods"""
+    return dir(obj)
