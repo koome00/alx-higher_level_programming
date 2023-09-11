0f336c2d40197b99bfae1b37e92ffd002d3c51e2
koome <koomemc@gmail.com>
2023-09-11 08:26:55 -0700

    initial commit

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+
+class MyList that inherits from list
+"""
+
+
+class MyList(list):
+    """
+    inherits from list
+
+    methods:
+    print_sorted(self)
+    """
+    def print_sorted(self):
+        """prints list of ints all sorted in ascending order"""
+        print(sorted(self))
