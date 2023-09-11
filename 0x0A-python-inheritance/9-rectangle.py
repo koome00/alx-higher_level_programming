21ffe1f870fec273d4361ee440fc091f5d40c4a1
koome <koomemc@gmail.com>
2023-09-11 15:42:31 -0700

    initial commit

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""
+Module 8-base_geometry
+
+Defines class BaseGeometry with
+public instance methods
+
+Defines a subclass Rectangle of class Base geometry
+"""
+
+
+class BaseGeometry():
+    """
+    Methods:
+        area(self)
+        integer_validator(self, name, value)
+    """
+    def area(self):
+        """ raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+        Attributes:
+            name(str) :will be a string
+            value(int) : integer
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+class Rectangle(BaseGeometry):
+    """
+    defines class rectangle
+
+    Methods:
+        __init__(self, width, height)
+        area(self)
+        __str__(self)
+
+    Attributes:
+        width (int): width of rectangle
+        height (int): height of rectangle
+    """
+    def __init__(self, width, height):
+        """
+        Initializer
+
+        Attributes:
+            width (int): rectangle width
+            height (int): rectangle height
+        """
+        super().integer_validator("width", width)
+        self.__width = width
+        super().integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """returns area"""
+        return self.__width * self.__height
+
+    def __str__(self):
+        """prints [Rectangle] <width>/<height>"""
+        return "[{:s}] {:d}/{:d}".format(self.__class__.__name__,
+                self.__width, self.__height)
