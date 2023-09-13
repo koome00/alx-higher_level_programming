54017f8124ae349bfa567608971376ff9c461887
koome <koomemc@gmail.com>
2023-09-13 15:46:22 -0700

    initial commit

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-load_from_json_file
+
+Contains a function  that creates an Object from a “JSON file”
+"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """
+     a function that creates an Object from a “JSON file
+    """
+    with open(filename, 'r', encoding="utf-8") as f:
+        return json.load(f)
