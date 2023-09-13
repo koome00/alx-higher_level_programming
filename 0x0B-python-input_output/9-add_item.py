8e3caa6e60b5448ba9e15c4bc5dbd67c892c139d
koome <koomemc@gmail.com>
2023-09-13 16:11:35 -0700

    initial commit

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Module 9-add_item
+
+Contains function that adds and saves to Python obj to JSON file; loads objects
+"""
+
+
+from sys import argv
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    existing_content = load_from_json_file(filename)
+except FileNotFoundError:
+    existing_content = []
+
+save_to_json_file(existing_content + argv[1:], filename)
